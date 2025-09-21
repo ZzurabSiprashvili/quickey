1482f6b07658fd96d1ff841c2ea97bbef09d5375
ZzurabSiprashvili <zz.siprashvili@gmail.com>
2025-09-21 23:23:24 +0400

    Initial Commit

---
+++ quickey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  quickey
+//
+//  Created by Zurab Siprashvili on 21.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ quickey/quickeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  quickeyApp.swift
+//  quickey
+//
+//  Created by Zurab Siprashvili on 21.09.25.
+//
+
+import SwiftUI
+
+@main
+struct quickeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
